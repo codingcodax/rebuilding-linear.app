492ab9233b2dc4576edc6131657367c1cbd013f3
Alexis Guzman <codingcodax@gmail.com>
2023-01-11 20:14:28 -0600

    chore(tailwind): add sans font

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,11 @@
 module.exports = {
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: '"SF Pro Display", -apple-system, BlinnMacSystemFonr, "Segoe UI", Roboto, Oxygen, Ubuntu, Canterall, "Open Sans", "Helvetica Neue", sans-serif',
+      },
+    },
   },
   plugins: [],
 };
