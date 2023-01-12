52b2e4cdcb2772a4a54847e26e99aacedfc260a8
Alexis Guzman <codingcodax@gmail.com>
2023-01-11 20:54:10 -0600

    chore(tailwind): replace theme instead of extend

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,18 +2,17 @@
 module.exports = {
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {
-      fontFamily: {
-        sans: '"SF Pro Display", -apple-system, BlinkMacSystemFonr, "Segoe UI", Roboto, Oxygen, Ubuntu, Cantarell, "Open Sans", "Helvetica Neue", sans-serif',
-      },
-      fontSize: {
-        md: "1.6rem",
-        lg: "2.2rem",
-        "5xl": "8rem",
-      },
-      colors: {
-        background: "#000212",
-      },
+    fontFamily: {
+      sans: '"SF Pro Display", -apple-system, BlinkMacSystemFonr, "Segoe UI", Roboto, Oxygen, Ubuntu, Cantarell, "Open Sans", "Helvetica Neue", sans-serif',
+    },
+    fontSize: {
+      md: "1.6rem",
+      lg: "2.2rem",
+      "5xl": "8rem",
+    },
+    colors: {
+      white: "#fff",
+      background: "#000212",
     },
   },
   plugins: [],
