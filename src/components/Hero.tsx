2322bee86db230834380cd796cf9c5476fa68a7a
Alexis Guzman <codingcodax@gmail.com>
2023-01-13 10:17:06 -0600

    feat(components): setup hero

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,9 +1,20 @@
-const Hero = () => {
-  return (
-    <div>
-      <p>hero</p>
-    </div>
-  );
+interface HeroProps {
+  children: React.ReactNode;
+}
+
+interface HeroElementProps {
+  children: React.ReactNode;
+}
+
+const Hero = ({ children }: HeroProps) => {
+  return <div className="text-center">{children}</div>;
 };
+
+const Title = ({ children }: HeroElementProps) => <h1>{children}</h1>;
+
+const Subtitle = ({ children }: HeroElementProps) => <h2>{children}</h2>;
+
+Hero.Title = Title;
+Hero.Subtitle = Subtitle;
 
 export default Hero;
