0575edc4e01521939b222b66062c29b81507d4d3
Alexis Guzman <codingcodax@gmail.com>
2023-01-13 12:43:50 -0600

    feat(hero): add text sizes to title and subtitle

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -10,9 +10,13 @@ const Hero = ({ children }: HeroProps) => {
   return <div className="text-center">{children}</div>;
 };
 
-const Title = ({ children }: HeroElementProps) => <h1>{children}</h1>;
+const Title = ({ children }: HeroElementProps) => (
+  <h1 className="text-5xl">{children}</h1>
+);
 
-const Subtitle = ({ children }: HeroElementProps) => <h2>{children}</h2>;
+const Subtitle = ({ children }: HeroElementProps) => (
+  <h2 className="text-lg">{children}</h2>
+);
 
 Hero.Title = Title;
 Hero.Subtitle = Subtitle;
