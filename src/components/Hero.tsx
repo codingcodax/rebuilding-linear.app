95b35cf2b91be1c036b58e57e0ed0eb63742a93c
Alexis Guzman <codingcodax@gmail.com>
2023-01-13 13:02:18 -0600

    feat(hero): add margins to title and subtitle

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -11,11 +11,11 @@ const Hero = ({ children }: HeroProps) => {
 };
 
 const Title = ({ children }: HeroElementProps) => (
-  <h1 className="text-5xl">{children}</h1>
+  <h1 className="my-6 text-5xl">{children}</h1>
 );
 
 const Subtitle = ({ children }: HeroElementProps) => (
-  <h2 className="text-lg">{children}</h2>
+  <h2 className="mb-11 text-lg">{children}</h2>
 );
 
 Hero.Title = Title;
