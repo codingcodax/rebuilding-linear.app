a99742076f4013ef7b1c5dc945f0da8194edcdb4
Alexis Guzman <codingcodax@gmail.com>
2023-01-18 13:50:45 -0600

    feat(app): add inter font

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,14 +1,25 @@
 import type { AppProps } from 'next/app';
+import { Inter } from '@next/font/google';
 
 import { Layout } from '~/components';
 
 import '~/styles/globals.css';
 
+const inter = Inter({ subsets: ['latin'], variable: '--font-inter' });
+
 const MyApp = ({ Component, pageProps }: AppProps) => {
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <>
+      {/* eslint-disable-next-line react/no-unknown-property */}
+      <style global jsx>{`
+        html {
+          font-family: ${inter.style.fontFamily};
+        }
+      `}</style>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </>
   );
 };
 
