6adbde87bf0ff708c5b0763fecab6a191fdd34ec
Alexis Guzman <codingcodax@gmail.com>
2023-01-18 13:58:57 -0600

    feat(header): setup

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,8 +1,68 @@
+import Link from 'next/link';
+
+import Icons from '~/components/Icons';
+
 const Header = () => {
   return (
-    <div>
-      <p>Header</p>
-    </div>
+    <header className='container fixed top-0 left-0 right-0 z-20 w-full px-8'>
+      <div className='before:pointer-events-none before:absolute before:-top-[1px] before:right-0 before:-bottom-1/2 before:left-0 before:select-none before:backdrop-blur-md before:[mask-image:linear-gradient(to_bottom,#000_4.8rem,transparent)]' />
+
+      <nav>
+        <ul className='relative flex h-12 items-center space-x-6 text-sm after:absolute after:bottom-0 after:h-[1px] after:w-full after:bg-white/[0.08]'>
+          <li>
+            <Link className='flex items-center text-md font-medium' href='/'>
+              <Icons.Logo className='mr-2 h-[1.8rem] w-[1.8rem]' /> Linear
+            </Link>
+          </li>
+          <li>
+            <Link className='nav-link' href='#'>
+              Features
+            </Link>
+          </li>
+          <li>
+            <Link className='nav-link' href='#'>
+              Method
+            </Link>
+          </li>
+          <li>
+            <Link className='nav-link' href='#'>
+              Customers
+            </Link>
+          </li>
+          <li>
+            <Link className='nav-link' href='#'>
+              Changelog
+            </Link>
+          </li>
+          <li>
+            <Link className='nav-link' href='#'>
+              Integrations
+            </Link>
+          </li>
+          <li>
+            <Link className='nav-link' href='#'>
+              Pricing
+            </Link>
+          </li>
+          <li>
+            <Link className='nav-link' href='#'>
+              Company
+            </Link>
+          </li>
+
+          <li className='ml-[auto_!important]'>
+            <Link className='nav-link' href='#'>
+              Log in
+            </Link>
+          </li>
+          <li>
+            <Link className='primary-button h-8' href='#'>
+              Sign up
+            </Link>
+          </li>
+        </ul>
+      </nav>
+    </header>
   );
 };
 
