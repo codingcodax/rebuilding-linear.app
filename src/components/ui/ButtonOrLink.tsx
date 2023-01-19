4a5f86b5b9a1741e5ab134d45bd9d01481831ca4
Alexis Guzman <codingcodax@gmail.com>
2023-01-18 19:51:22 -0600

    feat(ui): setup button or link

--- src/components/ui/ButtonOrLink.tsx
+++ src/components/ui/ButtonOrLink.tsx
@@ -1,11 +1,19 @@
-import React from 'react';
+import type { ComponentProps } from 'react';
+import Link from 'next/link';
 
-const ButtonOrLink = () => {
-  return (
-    <div>
-      <p>Button Or Link component</p>
-    </div>
-  );
+type ButtonOrLinkProps = ComponentProps<'button'> & ComponentProps<typeof Link>;
+
+export type Props = ButtonOrLinkProps;
+
+export const ButtonOrLink = ({ href, ...props }: Props) => {
+  const isLink = typeof href !== 'undefined';
+  const ButtonOrLink = isLink ? 'a' : 'button';
+
+  const content = <ButtonOrLink {...props} />;
+
+  if (isLink) return <Link href={href} {...props} />;
+
+  return content;
 };
 
 export default ButtonOrLink;
