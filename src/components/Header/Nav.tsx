0bcfd79c33fbacc3c69f76691aa189079e3b2ecd
Alexis Guzman <codingcodax@gmail.com>
2023-01-24 13:02:34 -0600

    refactor(nav): split classname for ul tag with cn utility

--- src/components/Header/Nav.tsx
+++ src/components/Header/Nav.tsx
@@ -1,5 +1,6 @@
-import { Button } from '~/components/ui/Button';
 import { navLinks } from '~/constants';
+import { cn } from '~/utils';
+import { Button } from '~/components/ui/Button';
 
 import Logo from './Logo';
 import NavItem from './NavItem';
@@ -9,7 +10,12 @@ import NavLink from './NavLink';
 const Nav = () => {
   return (
     <nav className='container z-10 px-6 md:px-8'>
-      <ul className='relative flex h-12 items-center space-x-6 text-sm after:absolute after:bottom-0 after:h-[1px] after:w-full after:bg-white/[0.08]'>
+      <ul
+        className={cn(
+          'relative flex h-12 items-center space-x-6 text-sm',
+          'after:absolute after:bottom-0 after:h-[1px] after:w-full after:bg-white/[0.08]'
+        )}
+      >
         <Logo />
 
         {navLinks
