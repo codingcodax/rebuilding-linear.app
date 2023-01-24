8e7b2603f6d5578f0eb6b9833b9af56b605fb8bb
Alexis Guzman <codingcodax@gmail.com>
2023-01-23 20:19:30 -0600

    feat(constants): create nav links data

---
+++ src/constants.ts
@@ -0,0 +1,54 @@
+export const navLinks = [
+  {
+    name: 'Features',
+    href: '#',
+    isInTablet: true,
+  },
+  {
+    name: 'Method',
+    href: '#',
+    isInTablet: true,
+  },
+  {
+    name: 'Customers',
+    href: '#',
+  },
+  {
+    name: 'Changelog',
+    href: '#',
+  },
+  {
+    name: 'Integrations',
+    href: '#',
+  },
+  {
+    name: 'Pricing',
+    href: '#',
+    isInTablet: true,
+  },
+  {
+    name: 'Company',
+    isInTablet: true,
+    isDropdown: true,
+    links: [
+      { name: 'About', href: '#' },
+      { name: 'Blog', href: '#' },
+      { name: 'Careers', href: '#' },
+    ],
+  },
+  {
+    name: 'About',
+    href: '#',
+    isMobile: true,
+  },
+  {
+    name: 'Blog',
+    href: '#',
+    isMobile: true,
+  },
+  {
+    name: 'Careers',
+    href: '#',
+    isMobile: true,
+  },
+];
