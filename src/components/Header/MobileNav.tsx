e5e0f620185ed68731a81ba7137cd17c6ad8f65f
Alexis Guzman <codingcodax@gmail.com>
2023-01-24 13:37:08 -0600

    refactor(mobilenav): split classname for ul tag with cn utility

--- src/components/Header/MobileNav.tsx
+++ src/components/Header/MobileNav.tsx
@@ -1,10 +1,16 @@
 import { navLinks } from '~/constants';
+import { cn } from '~/utils';
 
 import NavItem from './NavItem';
 
 const MobileNav = () => {
   return (
-    <ul className='grid h-[calc(100vh_-_4.8rem)] grid-flow-row grid-rows-[repeat(9,_4.8rem)] content-start divide-y divide-grey-dark text-md md:hidden'>
+    <ul
+      className={cn(
+        'grid h-[var(--mobile-nav-height)] grid-flow-row grid-rows-[repeat(9,_4.8rem)] content-start divide-y divide-grey-dark text-md',
+        'md:hidden'
+      )}
+    >
       {navLinks
         .filter((link) => link.isDropdown !== true)
         .map(({ name, href }) => (
