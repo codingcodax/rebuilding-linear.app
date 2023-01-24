24d2047927938c2698641b766fa97eea047fe31f
Alexis Guzman <codingcodax@gmail.com>
2023-01-23 20:23:46 -0600

    feat(header): setup logo

--- src/components/Header/Logo.tsx
+++ src/components/Header/Logo.tsx
@@ -1,8 +1,13 @@
+import Link from 'next/link';
+
+import Icons from '~/components/Icons';
 const Logo = () => {
   return (
-    <div>
-      <p>Logo</p>
-    </div>
+    <li>
+      <Link className='flex items-center text-md font-medium' href='/'>
+        <Icons.Logo className='mr-2 h-[1.8rem] w-[1.8rem]' /> Linear
+      </Link>
+    </li>
   );
 };
 
