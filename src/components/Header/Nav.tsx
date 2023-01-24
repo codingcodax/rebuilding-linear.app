5bb0a1aa2a4add569499a9bc660a4b8ebc5a0cb0
Alexis Guzman <codingcodax@gmail.com>
2023-01-24 12:57:13 -0600

    feat(header): setup nav

--- src/components/Header/Nav.tsx
+++ src/components/Header/Nav.tsx
@@ -1,8 +1,35 @@
+import { Button } from '~/components/ui/Button';
+import { navLinks } from '~/constants';
+
+import Logo from './Logo';
+import NavItem from './NavItem';
+import MobileNav from './MobileNav';
+import NavLink from './NavLink';
+
 const Nav = () => {
   return (
-    <div>
-      <p>Nav</p>
-    </div>
+    <nav className='container z-10 px-6 md:px-8'>
+      <ul className='relative flex h-12 items-center space-x-6 text-sm after:absolute after:bottom-0 after:h-[1px] after:w-full after:bg-white/[0.08]'>
+        <Logo />
+
+        {navLinks
+          .filter((link) => link.isMobile !== true)
+          .map(({ name, href, isInTablet }) => (
+            <NavItem key={name} href={href} isInTablet={isInTablet}>
+              {name}
+            </NavItem>
+          ))}
+
+        <li className='ml-[auto_!important]'>
+          <NavLink href='#'>Log in</NavLink>
+        </li>
+        <li>
+          <Button href='#'>Sign up</Button>
+        </li>
+      </ul>
+
+      <MobileNav />
+    </nav>
   );
 };
 
