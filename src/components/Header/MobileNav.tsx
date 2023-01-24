ddff71c0cb60efb36bd4bedc71a22a54ec2a1579
Alexis Guzman <codingcodax@gmail.com>
2023-01-24 12:57:22 -0600

    feat(header): setup mobile nav

--- src/components/Header/MobileNav.tsx
+++ src/components/Header/MobileNav.tsx
@@ -1,8 +1,18 @@
+import { navLinks } from '~/constants';
+
+import NavItem from './NavItem';
+
 const MobileNav = () => {
   return (
-    <div>
-      <p>MobileNav</p>
-    </div>
+    <ul className='grid h-[calc(100vh_-_4.8rem)] grid-flow-row grid-rows-[repeat(9,_4.8rem)] content-start divide-y divide-grey-dark text-md md:hidden'>
+      {navLinks
+        .filter((link) => link.isDropdown !== true)
+        .map(({ name, href }) => (
+          <NavItem key={name} href={href} isMobile={true}>
+            {name}
+          </NavItem>
+        ))}
+    </ul>
   );
 };
 
