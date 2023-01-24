c4fe4955e157e66643570e92aed4bb07dee157e7
Alexis Guzman <codingcodax@gmail.com>
2023-01-24 12:56:45 -0600

    feat(header): setup nav item

--- src/components/Header/NavItem.tsx
+++ src/components/Header/NavItem.tsx
@@ -1,8 +1,26 @@
-const NavItem = () => {
+import { cn } from '~/utils';
+
+import NavLink from './NavLink';
+
+interface Props {
+  href?: string;
+  isInTablet?: boolean;
+  isMobile?: boolean;
+  children: React.ReactNode;
+}
+
+const NavItem = ({ href, isInTablet, isMobile = false, children }: Props) => {
   return (
-    <div>
-      <p>Nav Item</p>
-    </div>
+    <li
+      className={cn(
+        !isMobile && 'hidden',
+        isInTablet ? 'md:list-item' : 'lg:list-item'
+      )}
+    >
+      {!href && <button>{children}</button>}
+
+      {href && <NavLink href={href}>{children}</NavLink>}
+    </li>
   );
 };
 
