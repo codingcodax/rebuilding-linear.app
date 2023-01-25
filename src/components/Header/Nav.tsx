03c6a90451a2fa16f7ae52dad9ba091dadead956
Alexis Guzman <codingcodax@gmail.com>
2023-01-25 11:29:43 -0600

    feat(nav): wrap ul tag with a div tag

--- src/components/Header/Nav.tsx
+++ src/components/Header/Nav.tsx
@@ -17,36 +17,38 @@ const Nav = () => {
   const handleMenu = () => setMenuIsOpen((prev) => !prev);
 
   return (
-    <nav className='container z-10 px-6 md:px-8'>
-      <ul
-        className={cn(
-          'relative flex h-12 items-center space-x-6 text-sm',
-          'after:absolute after:bottom-0 after:h-[1px] after:w-full after:bg-white/[0.08]'
-        )}
-      >
-        <Logo />
+    <nav className='container z-10'>
+      <div className='px-6 md:px-8'>
+        <ul
+          className={cn(
+            'relative flex h-12 items-center space-x-6 text-sm',
+            'after:absolute after:bottom-0 after:h-[1px] after:w-full after:bg-white/[0.08]'
+          )}
+        >
+          <Logo />
 
-        {navLinks
-          .filter((link) => link.isMobile !== true)
-          .map(({ name, href, isInTablet }) => (
-            <NavItem key={name} href={href} isInTablet={isInTablet}>
-              {name}
-            </NavItem>
-          ))}
+          {navLinks
+            .filter((link) => link.isMobile !== true)
+            .map(({ name, href, isInTablet }) => (
+              <NavItem key={name} href={href} isInTablet={isInTablet}>
+                {name}
+              </NavItem>
+            ))}
 
-        <li className='!ml-auto'>
-          <NavLink href='/'>Log in</NavLink>
-        </li>
-        <li>
-          <Button href='/'>Sign up</Button>
-        </li>
+          <li className='!ml-auto'>
+            <NavLink href='/'>Log in</NavLink>
+          </li>
+          <li>
+            <Button href='/'>Sign up</Button>
+          </li>
 
-        <button onClick={handleMenu}>
-          <Hamburger className='h-[1.8rem] w-[1.8rem]' />
-        </button>
-      </ul>
+          <button className='md:hidden' onClick={handleMenu}>
+            <Hamburger className='h-[1.8rem] w-[1.8rem]' />
+          </button>
+        </ul>
+      </div>
 
-      {menuIsOpen && <MobileNav />}
+      <MobileNav isOpen={menuIsOpen} />
     </nav>
   );
 };
