c58afd6f4ca82404d3b91bc6d59c3fd2a75130a0
Alexis Guzman <codingcodax@gmail.com>
2023-01-24 18:00:48 -0600

    feat(icons): add hamburger

---
+++ src/components/Icons/Hamburger.tsx
@@ -0,0 +1,7 @@
+const Hamburger = ({ className }: { className?: string }) => (
+  <svg className={className} fill='none' viewBox='0 0 18 11'>
+    <path d='M0 0h18v1H0V0Zm0 10h18v1H0v-1Z' fill='#fff' />
+  </svg>
+);
+
+export default Hamburger;
