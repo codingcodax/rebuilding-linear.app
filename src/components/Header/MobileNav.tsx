2d69f6f73adf4f5b1e0e3f70735eabd6508b09c5
Alexis Guzman <codingcodax@gmail.com>
2023-01-25 11:29:58 -0600

    feat(mobilenav): add animations when is open

--- src/components/Header/MobileNav.tsx
+++ src/components/Header/MobileNav.tsx
@@ -3,22 +3,36 @@ import { cn } from '~/utils';
 
 import NavItem from './NavItem';
 
-const MobileNav = () => {
+interface Props {
+  isOpen: boolean;
+}
+
+const MobileNav = ({ isOpen }: Props) => {
   return (
-    <ul
+    <div
       className={cn(
-        'grid h-[var(--mobile-nav-height)] grid-flow-row grid-rows-[repeat(9,_4.8rem)] content-start divide-y divide-grey-dark text-md',
-        'md:hidden'
+        'bg-background',
+        'transition-opacity duration-[400ms]',
+        'md:hidden',
+        isOpen ? 'opacity-100' : 'opacity-0'
       )}
     >
-      {navLinks
-        .filter((link) => link.isDropdown !== true)
-        .map(({ name, href }) => (
-          <NavItem key={name} href={href} isMobile={true}>
-            {name}
-          </NavItem>
-        ))}
-    </ul>
+      <ul
+        className={cn(
+          'grid h-[var(--mobile-nav-height)] grid-flow-row grid-rows-[repeat(9,_4.8rem)] content-start divide-y divide-grey-dark px-6 text-md',
+          'transition-transform duration-[400ms]',
+          isOpen ? 'translate-y-0' : 'translate-y-3'
+        )}
+      >
+        {navLinks
+          .filter((link) => link.isDropdown !== true)
+          .map(({ name, href }) => (
+            <NavItem key={name} href={href} isMobile={true}>
+              {name}
+            </NavItem>
+          ))}
+      </ul>
+    </div>
   );
 };
 
