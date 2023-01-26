8db43b2aca65a4318f73c8f17d0ea36bfb970e59
Alexis Guzman <codingcodax@gmail.com>
2023-01-26 16:03:31 -0600

    feat(icons): export github, slack and twitter

--- src/components/Icons/index.tsx
+++ src/components/Icons/index.tsx
@@ -1,5 +1,8 @@
+import GitHub from './GitHub';
 import Logo from './Logo';
+import Slack from './Slack';
+import Twitter from './Twitter';
 
-const Icons = { Logo };
+const Icons = { GitHub, Logo, Slack, Twitter };
 
 export default Icons;
